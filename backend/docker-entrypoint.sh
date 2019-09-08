1e6bee763f4640812121dad4376837d57cfd5f5d
Nikolay Bely <belyynikolay@gmail.com>
2019-09-09 01:43:51 +0700

    Add postgres run check

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,18 +1,48 @@
-#! /bin/sh
+#! /usr/bin/env sh
 
-echo Migrating
+set -o errexit
+set -o pipefail
+cmd="$@"
+
+function postgres_ready(){
+python << END
+import sys
+import psycopg2
+import environ
+try:
+    env = environ.Env()
+    dbname = env.str('POSTGRES_DB')
+    user = env.str('POSTGRES_USER')
+    password = env.str('POSTGRES_PASSWORD')
+    host = env.str('POSTGRES_HOST')
+    port = env.str('POSTGRES_PORT')
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing..."
+
+>&2 echo "Migrating..."
 python manage.py migrate
 
-echo Collect static
+>&2 echo "Collect static..."
 python manage.py collectstatic --noinput
 
 
 if [[ ${DEBUG} == 'TRUE' ]] || [[ ${DEBUG} == 'True' ]] || [[ ${DEBUG} == '1' ]]
 then
-  echo Starting debug server...
+  >&2 echo "Starting debug server..."
   exec python manage.py runserver 0.0.0.0:8000
 else
-    echo Starting Gunicorn...
+    >&2 echo "Starting Gunicorn..."
     exec gunicorn checklists.wsgi:application \
       -k egg:meinheld#gunicorn_worker \
       --name checklists \
