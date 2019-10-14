e4ed01d26ccc0828d8068060bdfad97a755a212a
Nikolay Bely <belyynikolay@gmail.com>
2019-10-14 10:13:49 +0700

    Add related sample

--- backend/rest_framework_cache/registry.py
+++ backend/rest_framework_cache/registry.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 import hashlib
+from collections import defaultdict
 
+from django.contrib.admin.utils import reverse_field_path
 from django.db.models import signals
 from django.utils.module_loading import autodiscover_modules
 from django.utils.six import text_type
@@ -13,14 +15,20 @@ from .exceptions import AlreadyRegistered
 class CacheRegistry:
     def __init__(self):
         self._registry = {}
+        self._related_registry = defaultdict(list)
 
-    def register(self, serializer, model=None):
+    def register(self, serializer, model=None, relations_to_track=None):
         """Store the serializer and model on registry to that the cache can be
         cleaned whenever an object is changed or deleted.
         After the serializer is registered we must connect the signals that
         clear the instance cache.
         can be used as a decorator
+
+        relations_to_track will allow you to designate further signals to trigger cache
+        clearing. relations_to_track uses the django queryset relation__field method for resolving.
         """
+        if relations_to_track is None:
+            relations_to_track = []
         if model is None:
             model = serializer.Meta.model
 
@@ -33,14 +41,27 @@ class CacheRegistry:
 
         self._registry[model].append(serializer)
         self.connect_signals(model)
+
+        for relation_str in relations_to_track:
+            related_model, lookup = reverse_field_path(model, relation_str)
+            self._related_registry[related_model].append(lookup)
+            self.connect_related_signals(related_model)
+
         return serializer
 
     def connect_signals(self, model):
         signals.post_save.connect(self.clear_instance, sender=model)
         signals.pre_delete.connect(self.clear_instance, sender=model)
 
+    def connect_related_signals(self, related_model):
+        signals.post_save.connect(self.clear_related_instance, sender=related_model)
+        signals.pre_delete.connect(self.clear_related_instance, sender=related_model)
+
     def get(self, model):
         return self._registry.get(model, [])
+
+    def get_related(self, model):
+        return self._related_registry.get(model, "")
 
     def get_all_cache_keys(self, instance):
         """Get all possibles cache keys for given instance"""
@@ -60,10 +81,19 @@ class CacheRegistry:
             serializer.__name__
         )).encode('utf-8')).hexdigest()
 
-    def clear_instance(self, sender, instance, **kwargs):
+    def clear_instance(self, instance, **kwargs):
         """Calls cache cleaner for current instance"""
         keys = self.get_all_cache_keys(instance)
         get_cache().delete_many(keys)
+
+    def clear_related_instance(self, instance, **kwargs):
+        """Calls cache cleaner for instance based on relation in registy"""
+        relations = cache_registry.get_related(self)
+        if cache_registry.get_related(self):
+            # Model has been registered as a relation
+            objects = self.objects.all().values_list(relations).distinct()
+            for obj in objects:
+                self.clear_instance(obj)
 
     def autodiscover(self):
         autodiscover_modules('serializers')
