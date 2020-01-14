70cc90c8d62b584851da6cc523bd649e57db08c5
DOXOPOKC <lilondin@gmail.com>
2020-01-14 10:21:01 +0700

    Fix env

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NODE_ENV === "production" ? "http://checklist.landfinance.ru" : "http://localhost";
+export const BASE_URL = process.env.NODE_ENV === "production" ? process.env.VUE_APP_DOMAIN_PROD : process.env.VUE_APP_DOMAIN_DEV;
 export default BASE_URL;
