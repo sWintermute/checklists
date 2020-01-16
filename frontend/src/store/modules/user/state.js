d66d89cad8615f06fdff21a37729fee3bb5e6847
DOXOPOKC <lilondin@gmail.com>
2020-01-16 09:26:51 +0700

    Fix profile && checklist

--- frontend/src/store/modules/user/state.js
+++ frontend/src/store/modules/user/state.js
@@ -1,10 +1,4 @@
 export default {
     auth_token: localStorage.getItem('auth_token') || '',
-    userProfile: {
-        id: "123",
-        email: "123",
-        first_name: "123",
-        last_name: "123",
-        position: "123"
-    },
+    userProfile: {},
 };
