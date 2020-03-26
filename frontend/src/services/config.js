a3a694a980cdd2f9475ff669d2016b78d1b58877
DOXOPOKC <lilondin@gmail.com>
2020-03-26 17:41:33 +0700

    Fix url config

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,6 +1,7 @@
 const protocol = process.env.VUE_APP_PROTOCOL
 const domain = process.env.VUE_APP_DOMAIN
+const port = process.env.VUE_APP_PORT
 
-const envApiUrl = `${protocol}://${domain}/`
+const envApiUrl = `${protocol}://${domain}${port ? ':' + port : ''}/`
 
 export const BASE_URL = envApiUrl
