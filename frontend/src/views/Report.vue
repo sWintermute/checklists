d6a66166ec48ed64747d99612dd91d28e3d30004
DOXOPOKC <lilondin@gmail.com>
2020-03-26 17:44:38 +0700

    Fix report space

--- frontend/src/views/Report.vue
+++ frontend/src/views/Report.vue
@@ -45,7 +45,7 @@
                                             v-list-item-title {{ note.created | date }}
                                             v-list-item-subtitle
                                                 template(v-for="key in note.keys")
-                                                    | {{ key.answer }}&nbsp;
+                                                    | {{ key.answer }};&nbsp;
 </template>
 
 <script>
