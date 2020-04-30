1779ed029ff6432e3e7b05b556da783d80d1c917
Nikolay Bely <belyynikolay@gmail.com>
2020-04-30 13:20:08 +0700

    Add draft of response email report generation

--- backend/notifications/tasks.py
+++ backend/notifications/tasks.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from post_office import mail
+from rest_framework.renderers import JSONRenderer
 
 
 def send_email(dest_email, title, message, html_message):
@@ -10,3 +11,22 @@ def send_email(dest_email, title, message, html_message):
         message=message,
         html_message=html_message,
     )
+
+
+def basic_report(response, answers, questions):
+    from lists import serializers
+    report = serializers.ReportSurveySerializer([response.survey],
+                                                responses=[response],
+                                                answers=answers,
+                                                questions=questions,
+                                                many=True).data
+    fl = False
+    for question in report[0]["questions"]:
+        if question["notes"] is None or question["notes"] is []:
+            fl = True
+
+    if fl:
+        send_email()
+
+    json = JSONRenderer().render(report)
+    print(json)
