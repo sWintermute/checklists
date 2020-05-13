417c684c59a271f691fc72e35623817bd2fe424f
DOXOPOKC <lilondin@gmail.com>
2020-05-13 10:02:22 +0700

    Fix autocomplete

--- frontend/src/views/FilledChecklist.vue
+++ frontend/src/views/FilledChecklist.vue
@@ -20,51 +20,68 @@
                     )
                         v-toolbar-title Ответ {{ filledList.id }}
                     v-card-text(class="px-6 pt-6 pb-0")
-                      v-form
-                        v-container
-                          v-row
-                            v-col(
-                              cols="12"
-                              v-for="(answer, i) in answers"
-                              :key="i"
-                            )
-                              template(v-if="answer.question.type === 'address-autocomplete'")
-                                autocomplete(
-                                  :id="answer.question.id"
-                                  :title="answer.question.text"
-                                )
-                              template(v-else-if="answer.question.type === 'textarea'")
-                                v-textarea(
-                                  solo
-                                  :label="answer.question.text"
-                                  class="mt-3"
-                                  v-model="answer.body"
-                                )
-                              template(v-else-if="answer.question.type === 'radio'")
-                                header {{ answer.question.text }}
-                                v-radio-group(v-model="answer.body")
-                                  v-radio(
-                                    v-for="n in answer.question.choices.split(';')"
-                                    :key="n"
-                                    :label="n"
-                                    :value="n"
-                                  )
-                              template(v-else)
-                                v-text-field(
-                                  v-model="answer.body"
-                                  :label="answer.question.text"
-                                )
-                            v-col(
-                              cols="12"
-                            )
-                              uploader(
-                                v-model="photo"
-                                title="Загрузите фото"
-                                :autoUpload="false"
+                      ValidationObserver(ref="observer" v-slot="{ handleSubmit }" tag="div")
+                        v-form(@submit.prevent="handleSubmit(UPDATE_FILLED_CHECKLIST)" id="check-login-form")
+                          v-container
+                            v-row
+                              v-col(
+                                cols="12"
+                                v-for="(answer, i) in answers"
+                                :key="i"
                               )
+                                template(v-if="answer.question.type === 'address-autocomplete'")
+                                  header {{ answer.question.text }}
+                                  ValidationProvider(:rules="answer.question.required ? 'required' : ''" v-slot="{ errors }")
+                                    v-autocomplete(
+                                      v-model="answer.body"
+                                      :label="answer.body || `Введите адрес...`"
+                                      :items="entries"
+                                      :search-input.sync="search"
+                                      :error-messages="errors"
+                                      color="white"
+                                      item-text="value"
+                                      item-value="value"
+                                      return-object
+                                      dense
+                                      full-width
+                                    )
+                                template(v-else-if="answer.question.type === 'textarea'")
+                                  v-textarea(
+                                    solo
+                                    :label="answer.question.text"
+                                    class="mt-3"
+                                    v-model="answer.body"
+                                  )
+                                template(v-else-if="answer.question.type === 'radio'")
+                                  header {{ answer.question.text }}
+                                  v-radio-group(v-model="answer.body")
+                                    v-radio(
+                                      v-for="n in answer.question.choices.split(';')"
+                                      :key="n"
+                                      :label="n"
+                                      :value="n"
+                                    )
+                                template(v-else)
+                                  v-text-field(
+                                    v-model="answer.body"
+                                    :label="answer.question.text"
+                                  )
+                              v-col(
+                                cols="12"
+                              )
+                                uploader(
+                                  v-model="photo"
+                                  title="Загрузите фото"
+                                  :autoUpload="false"
+                                )
                     v-card-actions
                       v-spacer
-                      v-btn(tile color="primary" @click="UPDATE_FILLED_CHECKLIST") Сохранить
+                      v-btn(
+                        tile
+                        type="submit"
+                        color="primary"
+                        form="check-login-form"
+                      ) Сохранить
 </template>
 
 <script>
@@ -83,23 +100,32 @@ export default {
     ValidationProvider,
     autocomplete
   },
-  data: () => ({}),
+  data: () => ({
+    search: null,
+  }),
   computed: {
     ...mapFields(`filledChecklists`, { photo: 'filledList.photo' }),
     ...mapMultiRowFields(`filledChecklists`, { answers: 'filledList.answers' }),
     ...mapState({
+      entries: state => state.checklists.entries,
       filledList: state => state.filledChecklists.filledList
     }),
   },
-  created () {
-    this.FETCH_FILLED_CHECKLIST({
+  watch: {
+    search (value) {
+      this.CHECKLIST_AUTOCOMPLETE_FIELD({ search: value })
+    }
+  },
+  async created () {
+    await this.FETCH_FILLED_CHECKLIST({
       id: this.$route.params.id
     })
   },
   methods: {
     ...mapActions({
       FETCH_FILLED_CHECKLIST: 'filledChecklists/FETCH_FILLED_CHECKLIST',
-      UPDATE_FILLED_CHECKLIST: 'filledChecklists/UPDATE_FILLED_CHECKLIST'
+      UPDATE_FILLED_CHECKLIST: 'filledChecklists/UPDATE_FILLED_CHECKLIST',
+      CHECKLIST_AUTOCOMPLETE_FIELD: 'checklists/CHECKLIST_AUTOCOMPLETE_FIELD'
     }),
   }
 }
