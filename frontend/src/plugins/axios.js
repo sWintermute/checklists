5ae7dc312068647c8bab4d793d461acacee60910
DOXOPOKC <lilondin@gmail.com>
2020-05-25 16:36:50 +0700

    Fix login redirect

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -10,12 +10,11 @@ Vue.axios.interceptors.response.use(function (response) {
   // Do something with response data
   return response
 }, function (error) {
-  console.log({ error })
   if (error.response.status === 401) {
     store.commit('SET_LOADING_STATUS', false)
     router.replace('/login')
-  } else {
-    router.go('/')
   }
+  // if (error.response.config.url.endsWith('login/')) {}
+  router.push('/')
   return Promise.reject(error)
 })
