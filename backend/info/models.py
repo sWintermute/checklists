a2d79b891425752bffd60ddeb3ab4388ee913f0f
Nikolay Bely <belyynikolay@gmail.com>
2020-08-05 08:30:56 +0700

    Add base tokenize command

--- backend/info/models.py
+++ backend/info/models.py
@@ -6,13 +6,15 @@ class TokenizedAdress(models.Model):
     """Model definition for TokenizedAdress."""
 
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    answer_body = models.CharField("Текст из ответа", max_length=250)
     unrestricted_value = models.CharField(
         "Адрес одной строкой (полный, с индексом)", max_length=250)
-    postal_code = models.CharField("Индекс", max_length=6)
-    region = models.CharField("Регион", max_length=50)
-    city = models.CharField("Город", max_length=50)
-    street = models.CharField("Улица", max_length=50)
-    house = models.CharField("Дом", max_length=50)
+    postal_code = models.CharField(
+        "Индекс", max_length=6, blank=True, null=True)
+    region = models.CharField("Регион", max_length=50, blank=True, null=True)
+    city = models.CharField("Город", max_length=50, blank=True, null=True)
+    street = models.CharField("Улица", max_length=50, blank=True, null=True)
+    house = models.CharField("Дом", max_length=50, blank=True, null=True)
 
     class Meta:
         verbose_name = 'TokenizedAdress'
